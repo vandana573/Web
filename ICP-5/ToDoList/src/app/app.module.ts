696d23b586a0e1e489c3854ea4bbf47ae5d1d5b6
vandana573 <mvandana666@gmail.com>
2021-09-26 21:51:23 -0500

    Create app.module.ts

---
+++ ICP-5/ToDoList/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TimerComponent } from './timer/timer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TimerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
